219d31fb0846c65d5ba597b77cc60ae67b1a5fe4
drice82 <xujiabin82@gmail.com>
2021-11-05 23:20:18 +0800

    Update nvidia-overclock.sh

--- nvidia-overclock.sh
+++ nvidia-overclock.sh
@@ -46,7 +46,7 @@ overclock () {
   # your particular system.
   log "Calling nvidia-settings to overclock GPU(s).."
   log "$(nvidia-smi -pl 140)"
-  log "$(nvidia-smi -lock-gpu-clocks=1200,1200)"
+  log "$(nvidia-smi -lgc 1215,1215)"
   log "$(nvidia-settings -c :0 -a '[gpu:0]/GPUGraphicsClockOffset[4]=-100' -a '[gpu:0]/GPUMemoryTransferRateOffset[4]=2000')"
   log "$(nvidia-settings -c :0 -a '[gpu:1]/GPUGraphicsClockOffset[4]=-100' -a '[gpu:1]/GPUMemoryTransferRateOffset[4]=2000')"
   log "$(nvidia-settings -c :0 -a '[gpu:2]/GPUGraphicsClockOffset[4]=-100' -a '[gpu:2]/GPUMemoryTransferRateOffset[4]=2000')"
